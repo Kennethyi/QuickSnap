d1ec0d6f2827db85f0c5afa858107656dff5731e
jerryl97 <chungchongsien@hotmail.com>
2017-09-11 20:45:55 +0800

    Changed text color to white according to pdf

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -54,7 +54,7 @@ namespace CardGames
 		/// Draws the game to the Window.
 		/// </summary>
 		/// <param name="myGame">The details of the game -- mostly top card and scores.</param>
-		private static void DrawGame(Snap myGame)
+		private static void DrawGame (Snap myGame)
 		{
 			SwinGame.DrawBitmap ("cardsBoard.png", 0, 0);
 
@@ -63,8 +63,8 @@ namespace CardGames
 			if (top != null)
 			{
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, "GameFont", 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, "GameFont", 0, 50);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, "GameFont", 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont", 0, 50);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
